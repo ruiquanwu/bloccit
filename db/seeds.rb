bde6194775accd4e3e0dea7b83cd8021bad61152
ruiquan wu <ruiquanwu@gmail.com>
2015-06-05 17:47:56 +0000

    completed Assignment unique Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
  require 'faker'
- 
+
+
  # Create Posts
  50.times do
    Post.create!(
@@ -16,7 +17,26 @@
      body: Faker::Lorem.paragraph
    )
  end
- 
+
+# create unique post.
+unique_post = "this is unique title"
+unique_comment = "this is unique comment"
+
+unless Post.where(title: unique_post).exists?
+  Post.create!(
+    title: unique_post,
+    body: "unqiue post body"
+    )
+end
+
+posts = Post.all
+
+unless Comment.where(body: unique_comment).exists?
+  Comment.create!(
+    post: posts.sample,
+    body: unique_comment
+    )
+end
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
