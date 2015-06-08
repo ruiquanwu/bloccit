cc77618d763e1fd3f9b81d66f590270683c285a1
ruiquan wu <ruiquanwu@gmail.com>
2015-06-08 22:55:43 +0000

    Assignment scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
-  default_scope { order('created_at DESC') }
+  #default_scope { order('created_at DESC') }
+  # define scope
+  scope :ordered_by_title, -> { order('title ASC') }
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
 end
