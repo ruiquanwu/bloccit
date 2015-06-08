934755918f4affe8f9c7e8c8daa0ea623e97cc76
ruiquan wu <ruiquanwu@gmail.com>
2015-06-08 22:22:21 +0000

    completed checkpoint associateions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  skip_before_action :flash_attack, only: [:new, :index]
   def index
      @posts = Post.all
   end
@@ -13,7 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
     if @post.save
       flash[:notice] = "Post was saved"
       redirect_to @post
