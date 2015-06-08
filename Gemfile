4f6e43a9037ae821ed0fe2265e666b905d5dddfb
ruiquan wu <ruiquanwu@gmail.com>
2015-06-08 23:49:57 +0000

    completed authorization

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'devise'
 
 gem 'figaro', '1.0'
 
+gem 'pundit'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608215447) do
+ActiveRecord::Schema.define(version: 20150608230816) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20150608215447) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
