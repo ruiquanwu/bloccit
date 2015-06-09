2dfae68352401427db4b36982b3e35e4a5f19935
ruiquan wu <ruiquanwu@gmail.com>
2015-06-09 21:46:20 +0000

    Interlude

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,8 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(post_params)
+    @post.topic_id = @post.id
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved"
@@ -33,7 +34,7 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
-    if @post.update_attributes(params.require(:post).permit(:title, :body))
+    if @post.update_attributes(post_params)
       flash[:notice] = "Post was updated."
       redirect_to [@topic, @post]
     else
@@ -41,4 +42,10 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+  
+  private
+
+def post_params
+  params.require(:post).permit(:title, :body)
+end
 end
