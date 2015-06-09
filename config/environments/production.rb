873d9ed971572ec07ca4a6698e5da76273aa1fbc
ruiquan wu <ruiquanwu@gmail.com>
2015-06-09 21:11:42 +0000

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+   config.action_mailer.default_url_options = { host: 'rocky-coast-8518.herokuapp.com' }
 end
