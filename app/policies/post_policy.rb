a04bbaeb3b71ef300a19a4a9000ca1a92e51622f
ruiquan wu <ruiquanwu@gmail.com>
2015-06-09 02:05:56 +0000

    completed assignment authorization

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -2,4 +2,21 @@
    def index?
      true
    end
+   
+   class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin? || user.moderator?
+        scope.all
+      else
+        scope.where(:user_id => user.id)
+      end
+    end
+   end
  end  
