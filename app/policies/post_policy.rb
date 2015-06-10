88852c282d5f7b8843a522ffe3237ac094c1d979
ruiquan wu <ruiquanwu@gmail.com>
2015-06-10 16:48:48 +0000

    fixed guest user

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -12,10 +12,14 @@
     end
 
     def resolve
-      if user.admin? || user.moderator?
-        scope.all
+      unless user.present?
+        scope.none
       else
-        scope.where(:user_id => user.id)
+        if user.admin? || user.moderator?
+          scope.all
+        else
+          scope.where(:user_id => user.id)
+        end
       end
     end
    end
