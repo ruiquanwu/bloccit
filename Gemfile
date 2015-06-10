322ebaa38d23f16d1f58759ba33d12ca6bb01ca7
ruiquan wu <ruiquanwu@gmail.com>
2015-06-10 18:37:08 +0000

    completed checkpoint-uploading-images

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'redcarpet'
 
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'fog'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609081910) do
+ActiveRecord::Schema.define(version: 20150610010259) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -46,8 +46,10 @@ ActiveRecord::Schema.define(version: 20150609081910) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.integer  "summary_id"
   end
 
+  add_index "posts", ["summary_id"], name: "index_posts_on_summary_id"
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
@@ -55,6 +57,12 @@ ActiveRecord::Schema.define(version: 20150609081910) do
     t.string   "title"
     t.text     "body"
     t.boolean  "resolved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "summaries", force: :cascade do |t|
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -86,6 +94,7 @@ ActiveRecord::Schema.define(version: 20150609081910) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
