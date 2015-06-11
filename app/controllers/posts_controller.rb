5f6f3ab8ca26e9947b60bd90ee97ce4a22dfaa34
ruiquan wu <ruiquanwu@gmail.com>
2015-06-11 16:00:46 +0000

    fixed @comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @topic = Topic.find(params[:topic_id])
     @comments = @post.comments
-    @comment = Comment.new
+    @comment = @post.comments.build
+   # raise @comment.inspect
   end
 
   def new
