3217420e604fc4235c4b7266e342ef56bb51c601
ruiquan wu <ruiquanwu@gmail.com>
2015-06-14 19:15:18 +0000

    completed checkpoint voting

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,9 +3,14 @@ describe Vote do
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect( Vote.new(value: 1).value).to eq(1)
-        expect( Vote.new(value: -1).value).to eq(-1)
-        expect( Vote.new(value: 13).value).to eq(nil)
+        v1 = Vote.new(value: 1)
+        expect( v1.valid? ).to eq(true)
+        
+        v2 = Vote.new(value: -1)
+        expect( v2.valid?).to eq(true)
+        
+        invalid_v = Vote.new(value: 13)
+        expect( invalid_v.valid?).to eq(false)
       end
     end
   end
