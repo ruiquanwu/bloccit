905eb2f58ba00fb7b5a42a92a3245eb199b60c07
skeletonx <ruiquanwu@gmail.com>
2015-06-15 17:34:01 +0000

    completed assignment private topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,6 @@
 class Topic < ActiveRecord::Base
-   has_many :posts, dependent: :destroy
-   scope :visible_to, -> (user) { user ? all : where(public: true) }
+  has_many :posts, dependent: :destroy
+  scope :visible_to, -> (user) { user ? all : publicly_viewable }
+  scope :publicly_viewable, -> { where(public: true) }
+  scope :privately_viewable, -> { where(public: false) }
 end
