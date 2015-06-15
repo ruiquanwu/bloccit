e69b380fed7f824ba42a7046a78b0cb48224ba46
skeletonx <ruiquanwu@gmail.com>
2015-06-15 20:50:12 +0000

    completed assignment public profiles

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,6 +3,8 @@
  describe "Visiting profiles" do
  
    include TestFactories
+   include Warden::Test::Helpers
+   Warden.test_mode!
  
    before do
      @user = authenticated_user
@@ -13,7 +15,6 @@
    end
  
    describe "not signed in" do
- 
      it "shows profile" do
        visit user_path(@user)
        expect(current_path).to eq(user_path(@user))
@@ -24,4 +25,21 @@
      end
  
    end
+   
+   describe "signed in" do
+     before do
+       login_as(@user, :scope => :user)
+     end
+     
+     it "shows profile" do
+     visit user_path(@user)
+     expect(current_path).to eq(user_path(@user))
+       
+     expect( page ).to have_content(@user.name)
+     expect( page ).to have_content(@post.title)
+     expect( page ).to have_content(@comment.body)
+     end
+   end
+   
+   
  end
